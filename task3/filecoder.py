fc5eb5cdc9f0765d5138c791449bbc72c83a441f
afrometal <radek.kwlsk@gmail.com>
2018-03-31 16:39:26 +0200

    split encoding and decoding bytes from files operations for use as a package

--- task3/filecoder.py
+++ task3/filecoder.py
@@ -24,66 +24,124 @@ NONCE_MODES = [AES.MODE_CCM, AES.MODE_EAX, AES.MODE_GCM,
 SEPARATOR = b'==='
 
 
-def encode(mode, key, files, challenge=False):
-    if challenge:
-        b = random.choice([0, 1])
-        files = [files[b]]
-    total_files = len(files)
-    for i, file_path in enumerate(files, start=1):
-        print("{}/{}: Encoding ...".format(i, total_files))
-        cipher, nonce = aes_encoder(mode, key)
-        if not challenge:
-            output_path = '.'.join([file_path, 'aes'])
-        else:
-            output_path = '/'.join([path.abspath(file_path).rsplit('/', maxsplit=1)[0], 'challenge.aes'])
-        with open(file_path, mode='rb') as file:
-            plain = file.read()
-            if mode == AES.MODE_CBC:
-                plain = Padding.pad(plain, AES.block_size, style='iso7816')
-            encrypted = cipher.encrypt(plain)
-        with open(output_path, mode='wb') as file:
-            output = SEPARATOR.join([nonce, encrypted])
-            file.write(output)
-        print("Successfully encoded into {}".format(output_path))
-
-
-def decode(mode, key, files):
-    total_files = len(files)
-    for i, file_path in enumerate(files, start=1):
-        print("{}/{}: Decoding {} ...".format(i, total_files, file_path))
-        with open(file_path, mode='rb') as file:
-            file_bytes = file.read()
-            nonce, ciphertext = file_bytes.split(SEPARATOR, maxsplit=1)
-            try:
-                cipher = aes_decoder(mode, key, nonce)
-                decrypted = cipher.decrypt(ciphertext)
-            except ValueError:
-                print("There was problem with decryption, make sure proper key and mode of operation is provided, "
-                      "program will now close")
+def encode_files(mode, key, files, challenge=False, iv=None, nonce=None):
+    try:
+        if challenge:
+            if len(files) != 2:
+                print("For challenge mode exactly 2 input files are required")
                 return
-        output_path = file_path.replace('.aes', '')
-        with open(output_path, mode='wb') as file:
-            try:
-                file.write(Padding.unpad(decrypted, AES.block_size, style='iso7816'))
-            except ValueError:
-                file.write(decrypted)
-        print("Successfully decoded into {}".format(output_path))
+            print("Encoding challenge ...")
+            output_path = '/'.join([path.abspath(files[0]).rsplit('/', maxsplit=1)[0], 'challenge.aes'])
+            with open(files[0], mode='rb') as file:
+                plain_1 = file.read()
+            with open(files[0], mode='rb') as file:
+                plain_2 = file.read()
+            (ciphertext, nonce), b = encode_challenge(mode, key, (plain_1, plain_2), iv=iv, nonce=nonce)
+            with open(output_path, mode='wb') as file:
+                output = SEPARATOR.join([nonce, ciphertext])
+                file.write(output)
+        else:
+            total_files = len(files)
+            for i, file_path in enumerate(files, start=1):
+                print("{}/{}: Encoding ...".format(i, total_files))
+                output_path = '.'.join([file_path, 'aes'])
+                with open(file_path, mode='rb') as file:
+                    plain = file.read()
+                ciphertext, nonce = encode(mode, key, plain, iv=iv, nonce=nonce)
+                with open(output_path, mode='wb') as file:
+                    output = SEPARATOR.join([nonce, ciphertext])
+                    file.write(output)
+                print("Successfully encoded into {}".format(output_path))
+    except (ValueError, TypeError) as e:
+        print("There was problem with encryption: {}, make sure proper key and mode of operation is provided, "
+              "program will now close".format(e))
+        return
+    except FileNotFoundError as e:
+        print("File '{}' not found, program will now close".format(e.filename))
+        return
 
 
-def aes_encoder(mode, key):
-    cipher = AES.new(key, mode)
-    if mode in IV_MODES:
-        return cipher, cipher.iv
-    else:
-        return cipher, cipher.nonce
+def encode_challenge(mode, key, plain_bytes_tuple, iv=None, nonce=None):
+    if type(plain_bytes_tuple) not in (list, tuple):
+        raise TypeError("Challenge requires list or tuple of two inputs")
+    if len(plain_bytes_tuple) != 2:
+        raise ValueError("Challenge requires exactly two inputs")
+    b = random.choice([0, 1])
+    plain_bytes = plain_bytes_tuple[b]
+    return encode(mode, key, plain_bytes, iv=iv, nonce=nonce), b
 
 
-def aes_decoder(mode, key, nonce):
+def encode(mode, key, plain_bytes, iv=None, nonce=None):
+    if mode == AES.MODE_CBC:
+        plain_bytes = Padding.pad(plain_bytes, AES.block_size, style='iso7816')
+    cipher, nonce = aes_encoder(mode, key, iv=iv, nonce=nonce)
+    ciphertext = cipher.encrypt(plain_bytes)
+    return ciphertext, nonce
+
+
+def decode_files(mode, key, files):
+    try:
+        total_files = len(files)
+        for i, file_path in enumerate(files, start=1):
+            print("{}/{}: Decoding {} ...".format(i, total_files, file_path))
+            with open(file_path, mode='rb') as file:
+                file_bytes = file.read()
+                nonce, ciphertext = file_bytes.split(SEPARATOR, maxsplit=1)
+                if mode in IV_MODES:
+                    iv = nonce
+                    nonce = None
+                else:
+                    iv = None
+                plain = decode(mode, key, ciphertext, iv=iv, nonce=nonce)
+            output_path = file_path.replace('.aes', '')
+            with open(output_path, mode='wb') as file:
+                try:
+                    file.write(Padding.unpad(plain, AES.block_size, style='iso7816'))
+                except ValueError:
+                    file.write(plain)
+            print("Successfully decoded into {}".format(output_path))
+    except ValueError as e:
+        print("There was problem with decryption: {}, make sure proper key and mode of operation is provided, "
+              "program will now close".format(e))
+        return
+    except FileNotFoundError as e:
+        print("File '{}' not found, program will now close".format(e.filename))
+        return
+
+
+def decode(mode, key, ciphertext_bytes, iv=None, nonce=None):
+    cipher = aes_decoder(mode, key, iv=iv, nonce=nonce)
+    plain = cipher.decrypt(ciphertext_bytes)
+    return plain
+
+
+def aes_encoder(mode, key, iv=None, nonce=None):
     if mode not in SUPPORTED_MODES.values():
         raise ValueError("Used mode is not supported")
     if mode in IV_MODES:
-        return AES.new(key, mode, iv=nonce)
+        if iv:
+            cipher = AES.new(key, mode, iv=iv)
+        else:
+            cipher = AES.new(key, mode)
+        return cipher, cipher.iv
     else:
+        if nonce:
+            cipher = AES.new(key, mode, nonce=nonce)
+        else:
+            cipher = AES.new(key, mode)
+        return cipher, cipher.nonce
+
+
+def aes_decoder(mode, key, iv=None, nonce=None):
+    if mode not in SUPPORTED_MODES.values():
+        raise ValueError("Used mode is not supported")
+    if mode in IV_MODES:
+        if not iv:
+            raise ValueError("IV has to be provided")
+        return AES.new(key, mode, iv=iv)
+    else:
+        if not nonce:
+            raise ValueError("Nonce has to be provided")
         return AES.new(key, mode, nonce=nonce)
 
 
@@ -101,6 +159,8 @@ def main():
     parser.add_argument('files', type=str, nargs='+', help='files to encode/decode')
     parser.add_argument('-s', type=str, dest='keystore_path', help='keystore path', default='keystore.jks')
     parser.add_argument('-k', type=str, dest='key_id', help='key identifier', default='myaeskey')
+    parser.add_argument('-i', type=str, dest='iv', help='predefined encoding iv', default=None)
+    parser.add_argument('-n', type=str, dest='nonce', help='predefined encoding nonce', default=None)
     parser.add_argument('-c', action='store_true', dest='challenge', help='challenge mode: on input m0, m1 your '
                                                                           'program picks independently, uniformly '
                                                                           'at random a bit `b` and returns '
@@ -111,20 +171,31 @@ def main():
     operation = args.operation
     mode = SUPPORTED_MODES[args.mode]
     files = args.files
+    iv = args.iv
+    nonce = args.nonce
     if operation == DECODE:
+        if iv or nonce:
+            print("Only encryption can use predefined iv/nonce, program will now close")
+            return
         for f in files:
             if not f.endswith('.aes'):
                 print("Only *.aes files are supported for decryption, program will now close")
                 return
+    if iv and nonce:
+        print("Either 'iv' or 'nonce' can be provided, not both, program will now close")
+        return
+    if iv and mode in NONCE_MODES:
+        print("Chosen encoding mode uses 'nonce' but 'iv' was provided, program will now close")
+        return
+    if nonce and mode in IV_MODES:
+        print("Chosen encoding mode uses 'iv' but 'nonce' was provided, program will now close")
+        return
     keystore_path = args.keystore_path
     key_id = args.key_id
     challenge = args.challenge
     if challenge:
         if mode == DECODE:
             print("Challenge mode available only in encryption")
-            return
-        if len(files) != 2:
-            print("For challenge mode exactly 2 input files are required")
             return
 
     keystore = None
@@ -150,9 +221,9 @@ def main():
             return
 
     if operation == ENCODE:
-        encode(mode, key.key, files, challenge=challenge)
+        encode_files(mode, key.key, files, challenge=challenge, iv=iv, nonce=nonce)
     elif operation == DECODE:
-        decode(mode, key.key, files)
+        decode_files(mode, key.key, files)
     else:
         print("Operation is invalid, accepted arguments are {}".format([ENCODE, DECODE]))
         return
